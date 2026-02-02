80b8b8d4cd2df270df6c73c3099a67b1f4466561
jairaj rathod <jarathod@bestpeers.in>
2026-02-02 18:06:44 +0530

    this is commit on project setup

--- Project Management Tool/project-management-tool/src/app/layout.tsx
+++ Project Management Tool/project-management-tool/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import MantineUIProvider from "@/components/providers/MantineProvider";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <MantineUIProvider>{children}</MantineUIProvider>
+        {children}
       </body>
     </html>
   );
